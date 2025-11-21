0be86fcb76aa9cc64702d214fa45a81db4577432
Ivo Popov <ivopopv@gmail.com>
2025-11-21 10:10:03 +0200

    Add AuthenticationHelper for user authentication; implement methods to retrieve user from headers

---
+++ src/main/java/com/divefinder/helpers/AuthenticationHelper.java
@@ -0,0 +1,71 @@
+package com.divefinder.helpers;
+
+
+import com.divefinder.models.User;
+import com.divefinder.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationHelper {
+    private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+    private static final String INVALID_AUTHENTICATION_ERROR = "Invalid authentication.";
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthenticationHelper(UserService userService) {
+        this.userService = userService;
+    }
+
+    public User tryGetUser(HttpHeaders headers) {
+        if (!headers.containsKey(AUTHORIZATION_HEADER_NAME)) {
+            throw new com.exceptions.AuthorizationException(INVALID_AUTHENTICATION_ERROR);
+        }
+
+        try {
+            String userInfo = headers.getFirst(AUTHORIZATION_HEADER_NAME);
+            String email = getEmail(userInfo);
+            String password = getPassword(userInfo);
+            User user = userService.findUserByEmail(email);
+
+            if (!user.getPassword().equals(password)) {
+                throw new com.exceptions.AuthorizationException(INVALID_AUTHENTICATION_ERROR);
+            }
+
+            return user;
+        } catch (com.exceptions.EntityNotFoundException e) {
+            throw new com.exceptions.AuthorizationException(INVALID_AUTHENTICATION_ERROR);
+        }
+    }
+
+    private String getEmail(String userInfo) {
+        int firstSpace = userInfo.indexOf(" ");
+        if (firstSpace == -1) {
+            throw new com.exceptions.AuthorizationException(INVALID_AUTHENTICATION_ERROR);
+        }
+
+        return userInfo.substring(0, firstSpace);
+    }
+
+    private String getPassword(String userInfo) {
+        int firstSpace = userInfo.indexOf(" ");
+        if (firstSpace == -1) {
+            throw new com.exceptions.AuthorizationException(INVALID_AUTHENTICATION_ERROR);
+        }
+
+        return userInfo.substring(firstSpace + 1);
+    }
+    //toDo will be implemented when MVC auth is implemented
+//    public User tryGetUser(HttpSession session){
+//        String currentUser = (String) session.getAttribute("currentUser");
+//        if (currentUser == null){
+//            throw new com.exceptions.AuthorizationException("No user logged in");
+//        }
+//        return userService.get(currentUser);
+//
+//    }
+
+}
